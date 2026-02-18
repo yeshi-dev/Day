9971c86d7fe21513ccbd6840854d8aa3f82fdd0a
yeshi-dev <liyanabarock@gmail.com>
2026-02-18 01:26:03 -0900

    add

--- App.js
+++ App.js
@@ -1,24 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import First from '/src/card'
-
-export default function App() {
-  return (
+import {View, Text, TextInput,Button ,stylesheet} from 'react-native';
+export default function App(){
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start done</Text>
-      <First />
-      <StatusBar style="auto" />
+    <Text style={styles.title}>My tasks</Text>
+    <Text>My tasks</Text>
+    <View style={styles.divider}/>
+    <View style ={styles.inputcontainer}/>
+    <Textinput
+     placeholder='add a new task....'
+     style={styles.inputText}
+      />
+
+     <Button
+     title='Add'
+     />
     </View>
   );
 }
+const styles =StyleSheet.create({
+container:{
+  flex:1,
+  flexDirection:'column',
+ backgroundcolor: '#fff',
+  alignItems: 'center',
+  justifycontent: 'center',
+},
+title:{
+  fontsize:20,
+  fontweight:'bold',
+},
+divider:{
+  width:'100%',
+height:2,
+backgroundcolor:'#c7c3c3',
+},
+inputcontainer:{
+  flexDirection:'row',
+  marginTop:2,
+  width:'80%',
+},
+inputText:{
+  flex: 1,
   
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+}
 });
