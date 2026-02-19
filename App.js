d14a5af447e983e778ec9371981c8e8f9e13f9ba
yeshi-dev <liyanabarock@gmail.com>
2026-02-19 08:42:55 -0900

    doing part 1 video

--- App.js
+++ App.js
@@ -1,48 +1,63 @@
-import {View, Text, TextInput,Button ,StyleSheet} from 'react-native';
-export default function App(){
+import {View,Text,StyleSheet, TouchableOpacity} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { TextInput } from 'react-native-paper';
+export default function APP(){
   return(
-    <View style={styles.container}>
-      <Text style={styles.title}>My tasks</Text>
-
-      <View style={styles.divider}/>
-
+    <SafeAreaView style={styles.container}>
       <View style ={styles.inputcontainer}>
         <TextInput
-        placeholder='add a new task....'
-        style={styles.inputText}
+          placeholder ="what would you like to focus...."
+          mode={"outlined"}
+          lable ="Focus"
+          style ={styles.InputText}
+          
           />
-
-        <Button
-        title='Add'
-        />
+       <TouchableOpacity style={styles.fabButton} onpress={()=>{}}>
+        <Text style={styles.fabText}>+</Text>
+        </TouchableOpacity>
       </View>
-    </View>
-  );
+      <View style={styles.focusedTasks}>
+        <Text style={styles.focusTitle}>Things we have focused on:</Text>
+        <View style={{padding:20}}>
+          <Text style={{fontSize:18, color:'white', fontWeight:'semi-bold'}}>1, learn react native</Text>
+          <Text style={{fontSize:18, color:'white', fontWeight:'semi-bold'}}>2, js basics</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  )
 }
-const styles = StyleSheet.create({
+const styles =StyleSheet.create({
 container:{
   flex:1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',
-},
-title:{
-  fontsize:20,
-  fontweight:'bold',
-  marginTop:100,
-},
-divider:{
-  marginTop:20,
-  width:'100%',
-  height:2,
-  backgroundcolor:'#c7c3c3',
+  backgroundcolor:'#2f0b68949',
 },
 inputcontainer:{
-  flexDirection:'row',
-  marginTop:20,
-  width:'80%',
-  borderWidth:1,
-}, 
-inputText:{
+  flexDirection :'row',
+  padding: 20,
+},
+InputText:{
+  flex:1,
+},
+fabButton:{
+  height:6,
+  width:60,
+  borderRadius:30,
+  justifyContent:'center',
+  alignItems:'center',
+  backgroundcolor:'transparent',
+  borderWidth:2,
+  borderColor:'white',
+  marginLeft:10,
+},
+fabText:{
+  fontSize:20,
+  color:'white',
+},
+focusTitle:{
+  fontWeight:'bold',
+  fontSize:22,
+  marginLeft:20,
+  color :'white',
 }
-});
+
+})
