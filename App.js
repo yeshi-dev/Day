10b6ffa166cf4585d30057d5214433ee2d27ddd8
yeshi-dev <liyanabarock@gmail.com>
2026-02-19 05:19:01 -0900

    correction

--- App.js
+++ App.js
@@ -1,46 +1,48 @@
-import {View, Text, TextInput,Button ,stylesheet} from 'react-native';
+import {View, Text, TextInput,Button ,StyleSheet} from 'react-native';
 export default function App(){
   return(
     <View style={styles.container}>
-    <Text style={styles.title}>My tasks</Text>
-    <Text>My tasks</Text>
-    <View style={styles.divider}/>
-    <View style ={styles.inputcontainer}/>
-    <Textinput
-     placeholder='add a new task....'
-     style={styles.inputText}
-      />
+      <Text style={styles.title}>My tasks</Text>
 
-     <Button
-     title='Add'
-     />
+      <View style={styles.divider}/>
+
+      <View style ={styles.inputcontainer}>
+        <TextInput
+        placeholder='add a new task....'
+        style={styles.inputText}
+          />
+
+        <Button
+        title='Add'
+        />
+      </View>
     </View>
   );
 }
-const styles =StyleSheet.create({
+const styles = StyleSheet.create({
 container:{
   flex:1,
-  flexDirection:'column',
- backgroundcolor: '#fff',
+  backgroundColor: '#fff',
   alignItems: 'center',
-  justifycontent: 'center',
+  justifyContent: 'center',
 },
 title:{
   fontsize:20,
   fontweight:'bold',
+  marginTop:100,
 },
 divider:{
+  marginTop:20,
   width:'100%',
-height:2,
-backgroundcolor:'#c7c3c3',
+  height:2,
+  backgroundcolor:'#c7c3c3',
 },
 inputcontainer:{
   flexDirection:'row',
-  marginTop:2,
+  marginTop:20,
   width:'80%',
-},
+  borderWidth:1,
+}, 
 inputText:{
-  flex: 1,
-  
 }
 });
