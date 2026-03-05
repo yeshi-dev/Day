bde3de0194f0d01d878aba39a93689a612821c89
yeshi-dev <liyanabarock@gmail.com>
2026-03-05 10:50:32 -0900

    update focusTime

--- App.js
+++ App.js
@@ -1,108 +1,113 @@
-import {View,Text,StyleSheet, TouchableOpacity} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { TextInput } from 'react-native-paper';
-import FocusTime from './componenets/FocusTime';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import { SafeAreaView  } from 'react-native-safe-area-context';
+import {TextInput } from 'react-native-paper';
 import { useState } from 'react';
+import FocusTime from './componenets/FocusTime';
 
-export default function APP(){
-  const [tasks, setTasks]=useState([])
-  const [task,setTask]=useState("");
-  const [selectedTask,setSelectedTask]=useState('');
-  const [switchScreen,setSwitchScreen]=useState(false);
+export default function App(){
 
-const addTask=()=>{
-const trimmed =task.trim();
-if(trimmed.length>0){
-  setTask(prev=>[...prev,trimmed]);
-  setTasks('');
-  setSelectedTask(trimmed);
+  const [switchScreen , setSwitchScreen ] = useState(false);
+  const [task , setTask ] = useState("");
+  const [tasks, setTasks] = useState([]); 
+  const [selectedTask, setSelectedTask] = useState("");
 
-}
-console.log(trimmed)
-}
-const changeScreen=()=>{
-  setSwitchScreen(!switchScreen);
-}
 
-if(switchScreen){
-  return <FocusTime focusTask={selectedTask}onBack={changeScreen}/>
-}
-  
+  const changeScreen = () => {
+    setSwitchScreen(!switchScreen);
+  }
+
+  const addTask = ( ) => {
+    const trimmed = task.trim();
+
+    if (trimmed.length > 0)
+    {
+      setTasks( prev =>[...prev, trimmed]);
+      setTask("");
+      setSelectedTask(trimmed);
+
+    }
+  }
+
+  if(switchScreen){
+    return <FocusTime focusTask={selectedTask} onBack={changeScreen}/>
+  }
+
+
   return(
-  
     <SafeAreaView style={styles.container}>
-      <View style ={styles.inputcontainer}>
+      <View style={styles.inputContainer}>
         <TextInput
-          placeholder ="what would you like to focus...."
-          mode={"outlined"} 
-          label ="Focus"
-          style ={styles.InputText}
-          value ={task}
-        
-          onChangeText={(Text)=>setTask(Text)}
-          />
-       <TouchableOpacity style={styles.fabButton} onPress={()=>{
-        changeScreen();
-        addTask();
-       }}>
-        <Text style={styles.fabText}>+</Text>
+          placeholder = "What would you like to focus ..."
+          mode = {"outlined"}
+          label = "Focus"
+          style = {styles.InputText} 
+          value = {task}
+          onChangeText = {(text)=> setTask(text)}
+
+        />
+        <TouchableOpacity style={styles.fabButton} onPress={() => {
+          addTask();
+          changeScreen();
+        }}>
+          <Text style={styles.fabText}>+</Text>
         </TouchableOpacity>
       </View>
+
       <View style={styles.focusedTasks}>
-        <Text style={styles.focusTitle}>Things we have focused on:</Text>
-        <View style={{padding:20}}>
-          {tasks.map((t,i)=>(
-            <Text key ={i}style={{color:'white',fontsize:18}}>{t}</Text>
-          ))}
-          <Text style={{fontSize:18, color:'white', fontWeight:'bold'}}>1, learn react native</Text>
-          <Text style={{fontSize:18, color:'white', fontWeight:'bold'}}>2, js basics</Text>
+        <Text style={styles.focusTitle}>  Things we've focused on: </Text>
+        {tasks.map((task,index) => (
+          <Text key={index} style={styles.taskText}>{task}</Text>
+        ))}
+        
+        <View style={{padding: 20}}>
+
         </View>
+
       </View>
     </SafeAreaView>
   )
 }
-const styles =StyleSheet.create({
-container:{
-  flex:1,
-  backgroundColor:'#2f0b68',
-},
-inputcontainer:{
-  flexDirection :'row',
-  padding: 20,
-},
-InputText:{
-  flex:1,
-},
-focusedTasks:{
-  marginTop:20,
-  flex:10,
-},
-fabButton:{
-  height:60,
-  width:60,
-  borderRadius:30,
-  justifyContent:'center',
-  alignItems:'center',
-  backgroundColor:'transparent',
-  borderWidth:2,
-  borderColor:'white',
-  marginLeft:10,
-},
-fabText:{
-  fontSize:20,
-  color:'white',
-},
-focusTitle:{
-  fontWeight:'bold',
-  fontSize:22,
-  marginLeft:20,
-  color :'white',
-},
-taskText:{
-  fontWeight:"semi-bold",
-  fontSize:18,
-  color:"white",
-padding:10,
-}
 
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#2d056d'
+  },
+  inputContainer:{
+    flexDirection: 'row',
+    padding: 20,
+  },
+  InputText:{
+    flex: 1,
+  },
+  fabButton: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    borderWidth: 2,
+    borderColor: 'white',
+    marginLeft: 10,
+  },
+  fabText:{
+    fontSize: 20,
+    color: 'white',
+  },
+  focusedTasks:{
+    marginTop: 20,
+  },
+  focusTitle:{
+    fontWeight: 'bold',
+    fontSize: 22,
+    marginLeft: 10,
+    color : 'white'
+  },
+  taskText: {
+    fontWeight: 'semi-bold',
+    frontSize: 18, 
+    color: 'white',
+    padding: 10,
+  }
 })
